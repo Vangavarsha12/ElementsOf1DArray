b51126911302a5384e774def9193766c3700a399
Vangavarsha12 <varshavanga38@gmail.com>
2025-01-26 19:02:07 +0530

    Add files via upload

---
+++ Array1D.java
@@ -0,0 +1,11 @@
+
+public class Array1D {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+
+        System.out.println("Elements in the array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
